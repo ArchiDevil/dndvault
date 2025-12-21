d1d47990031e20c38b804a11fb682bd4edc69c7e
Denis Bezykornov <archidevil52@gmail.com>
2025-12-22 00:18:14 +0300

    Add simple analytics

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -9,4 +9,19 @@ export default defineNuxtConfig({
   runtimeConfig: {
     backendAddress: 'http://directus:8055',
   },
+  app: {
+    head: {
+      title: 'DnD Worlds',
+      htmlAttrs: {
+        lang: 'ru',
+      },
+      script: [
+        {
+          src: 'https://stats.codecliffs.ru/script.js',
+          defer: true,
+          'data-website-id': 'a10ae2b7-497b-4b10-8b21-e2c1dac0bad3',
+        },
+      ],
+    },
+  },
 })
