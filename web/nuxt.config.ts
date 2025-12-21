18fcebfea32e41723b1b18960b2c5a3921b546a2
Denis Bezykornov <archidevil52@gmail.com>
2025-12-22 02:13:07 +0300

    Update endpoint names

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
   },
   compatibilityDate: '2025-12-21',
   runtimeConfig: {
-    backendAddress: 'http://directus:8055',
+    backendAddress: 'http://backend:8055',
   },
   app: {
     head: {
