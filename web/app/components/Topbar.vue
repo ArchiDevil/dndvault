57ac395a5eb7273c1231bb4b73aaffbd6a13eaf3
Denis Bezykornov <archidevil52@gmail.com>
2025-12-22 23:23:23 +0300

    Make nav smaller

--- web/app/components/Topbar.vue
+++ web/app/components/Topbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="my-8 py-4 flex flex-row gap-8 border-b-2 font-semibold">
+  <nav class="my-2 md:my-8 py-2 md:py-4 flex flex-row gap-8 border-b-2 font-semibold">
     <NuxtLink
       class="text-xl underline-offset-4 decoration-2 hover:underline"
       to="/">
