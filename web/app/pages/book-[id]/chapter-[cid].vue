f434b991247133d86b59133cf2af098e11eae41d
Denis Bezykornov <archidevil52@gmail.com>
2025-12-22 18:02:38 +0300

    Restyle chapters to look better

--- web/app/pages/book-[id]/chapter-[cid].vue
+++ web/app/pages/book-[id]/chapter-[cid].vue
@@ -4,14 +4,12 @@ import {createDirectives} from 'marked-directive'
 import type {BackendResponse, ChapterData} from '@@/interfaces'
 
 const route = useRoute()
-const chapterId = ref(route.params.cid)
 const bookId = ref(route.params.id)
-const backlink = computed(() => `/book-${bookId.value}/`)
-const toc: {text: string; level: number; link: string}[] = []
 
 const isHeading = (token: Tokens.Generic): token is Tokens.Heading =>
   token.type === 'heading'
 
+const toc: {text: string; level: number; link: string}[] = []
 marked
   .use({
     extensions: [
@@ -32,6 +30,7 @@ marked
   })
   .use(createDirectives())
 
+const chapterId = ref(route.params.cid)
 const response = await useFetch<BackendResponse<ChapterData>>(
   `/api/items/chapters/${chapterId.value}`
 )
@@ -46,6 +45,8 @@ onMounted(() => {
     content.value.innerHTML = renderedContent
   }
 })
+
+const backlink = computed(() => `/book-${bookId.value}/`)
 </script>
 
 <template>
@@ -79,83 +80,118 @@ onMounted(() => {
       </ul>
     </div>
     <div
-      class="container-content max-w-600"
+      class="chapter-content max-w-600"
       ref="content" />
   </article>
 </template>
 
-<style scoped>
-.container-content :deep(h1) {
-  @apply text-4xl;
-  @apply font-semibold;
-  @apply mt-8 mb-2;
-}
+<style>
+.chapter-content {
+  @apply text-slate-800;
 
-.container-content :deep(h2) {
-  @apply text-2xl;
-  @apply font-semibold;
-  @apply mt-4 mb-2;
-  @apply border-b-2 border-solid border-gray-500;
-}
+  h1,
+  h2,
+  h3,
+  h4 {
+    @apply font-semibold;
+  }
 
-.container-content :deep(h3) {
-  @apply text-xl;
-  @apply font-semibold;
-  @apply mt-4;
-}
+  h1 {
+    @apply text-4xl mb-2;
+  }
 
-.container-content :deep(h4) {
-  @apply text-lg;
-  @apply font-semibold;
-  @apply mt-4;
-}
+  h2 {
+    @apply text-2xl;
+    @apply mt-4;
+  }
+  h3 {
+    @apply text-xl;
+    @apply mt-4;
 
-.container-content :deep(blockquote) {
-  @apply border-red-900 border border-solid;
-  @apply p-4 my-4;
-  @apply bg-gray-100;
-  @apply text-sm;
-}
+    &::after {
+      @apply h-0.5 w-full content-[""] block bg-gray-500;
+    }
+  }
+  h4 {
+    @apply text-lg;
+    @apply mt-4;
+  }
 
-.container-content :deep(ul) {
-  @apply my-2 pl-4;
-}
+  ul {
+    @apply my-2 pl-4;
 
-.container-content :deep(li) {
-  @apply list-disc list-outside;
-}
+    li {
+      @apply list-disc list-outside;
+    }
+  }
 
-.container-content :deep(table) {
-  @apply mb-6;
-}
+  table {
+    @apply mb-6;
 
-.container-content :deep(th),
-.container-content :deep(td) {
-  @apply border border-solid border-zinc-500;
-  @apply py-1 px-2;
-}
+    th {
+      @apply text-left;
+    }
 
-.container-content :deep(th) {
-  @apply text-left;
-}
+    th,
+    td {
+      @apply py-1 px-2;
+    }
 
-.container-content :deep(p) {
-  @apply mb-2;
-}
+    tbody {
+      tr:nth-of-type(2n) {
+        @apply bg-blue-50 hover:bg-blue-200;
+      }
 
-.container-content :deep(p:last-child) {
-  @apply m-0;
-}
+      tr:nth-of-type(2n-1) {
+        @apply bg-blue-100 hover:bg-blue-200;
+      }
+    }
+  }
 
-.container-content :deep(.sidebar) {
-  @apply bg-zinc-300;
-  @apply border border-solid border-zinc-400;
-  @apply p-4;
-}
+  p {
+    @apply mb-2;
 
-.container-content :deep(.sidebar h1) {
-  @apply text-2xl;
-  @apply font-semibold;
-  @apply mt-0 mb-2;
+    &:last-child {
+      @apply m-0;
+    }
+  }
+
+  .first-paragraph {
+    p::first-line {
+      @apply uppercase italic;
+    }
+
+    p::first-letter {
+      @apply float-left text-[3.25rem] mr-[0.3rem] mt-[0.35rem] leading-[0.9];
+    }
+  }
+
+  blockquote {
+    @apply border-red-900 border border-solid;
+    @apply p-4 my-4;
+    @apply bg-gray-100;
+    @apply text-sm;
+  }
+
+  .sidebar {
+    @apply bg-zinc-300;
+    @apply border border-solid border-zinc-400;
+    @apply p-4;
+
+    h1 {
+      @apply text-xl;
+      @apply font-semibold;
+      @apply mt-0 mb-2;
+    }
+  }
+
+  .rev-pad-list {
+    ul {
+    }
+
+    li {
+      @apply list-none -indent-4;
+    }
+  }
 }
 </style>
