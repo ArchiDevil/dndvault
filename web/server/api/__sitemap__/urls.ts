1ee9f46529694c903c848916e0fad87969803a0c
Denis Bezykornov <archidevil52@gmail.com>
2025-12-26 00:02:50 +0300

    Add last modification time to sitemap

--- web/server/api/__sitemap__/urls.ts
+++ web/server/api/__sitemap__/urls.ts
@@ -1,37 +1,44 @@
 import {defineSitemapEventHandler} from '#imports'
 import type {SitemapUrlInput} from '#sitemap/types'
-import {type ChapterData, type BookData} from '@@/interfaces'
 
 export default defineSitemapEventHandler(async () => {
-  // TODO: there should be lastmod set to optimize crawling
   const output: SitemapUrlInput[] = []
-  const books = await $fetch<{data: BookData[]}>('/api/items/books')
+  const books = await $fetch<{
+    data: {id: number; slug: string; date_updated: string}[]
+  }>('/api/items/books', {
+    query: {
+      fields: 'id,slug,date_updated',
+    },
+  })
   for (const book of books.data) {
     // TODO: make it simpler, there are too many requests for now
-    const chapters = await $fetch<{data: ChapterData[]}>(
-      '/api/items/chapters',
-      {
-        query: {
-          filter: {
-            book_id: {
-              _eq: book.id,
-            },
+    const chapters = await $fetch<{
+      data: {id: number; date_updated: string}[]
+    }>('/api/items/chapters', {
+      query: {
+        filter: {
+          book_id: {
+            _eq: book.id,
           },
-          fields: 'id',
         },
-      }
-    )
+        fields: 'id,date_updated',
+      },
+    })
     for (const chapter of chapters.data) {
+      const lastUpdate = new Date(chapter.date_updated)
       output.push({
         loc: `/book-${book.slug}/chapter-${chapter.id}`,
         changefreq: 'weekly',
+        lastmod: lastUpdate,
         _sitemap: 'pages',
       })
     }
 
+    const lastBookUpdate = new Date(book.date_updated)
     output.push({
       loc: `/book-${book.slug}`,
       changefreq: 'monthly',
+      lastmod: lastBookUpdate,
       _sitemap: 'pages',
     })
   }
