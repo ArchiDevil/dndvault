34733ad47abcd97db113e62f88983ff96a82a6ef
Denis Bezykornov <archidevil52@gmail.com>
2025-12-26 16:02:51 +0300

    Use book API on books page

--- web/app/pages/book-[slug]/index.vue
+++ web/app/pages/book-[slug]/index.vue
@@ -1,38 +1,19 @@
 <script setup lang="ts">
 import type {
   BackendArrayResponse,
-  BookData,
   ChapterData,
 } from '#shared/types/backendTypes'
 
 const route = useRoute()
 const slug = ref(route.params.slug)
 
-const booksResponse = await useFetch<BackendArrayResponse<BookData>>(
-  `/api/items/books/`,
-  {
-    query: {
-      filter: {
-        slug: {
-          _eq: slug.value,
-        },
-      },
-    },
-  }
-)
-
-if (
-  !booksResponse.data.value ||
-  !booksResponse.data.value.data ||
-  !booksResponse.data.value.data.length
-) {
+const {data: bookData} = await useFetch(`/api/books/${slug.value}`)
+if (!bookData.value) {
   throw createError({
     status: 404,
     statusText: 'Page not found :(',
   })
 }
-
-const bookData = booksResponse.data.value.data[0]!
 
 const chaptersResponse = await useFetch<BackendArrayResponse<ChapterData>>(
   `/api/items/chapters`,
@@ -40,7 +21,7 @@ const chaptersResponse = await useFetch<BackendArrayResponse<ChapterData>>(
     query: {
       filter: {
         book_id: {
-          _eq: bookData.id,
+          _eq: bookData.value.id,
         },
       },
       sort: 'sort',
@@ -51,19 +32,19 @@ const chaptersResponse = await useFetch<BackendArrayResponse<ChapterData>>(
 const chapters = chaptersResponse.data.value!.data
 
 useSeoMeta({
-  title: `DnD Vault - ${bookData.title}`,
-  description: `Оглавление и краткое описание книги ${bookData.title}`,
-  ogTitle: `DnD Vault - ${bookData.title}`,
-  ogDescription: `Оглавление и краткое описание книги ${bookData.title}`,
+  title: `DnD Vault - ${bookData.value.title}`,
+  description: `Оглавление и краткое описание книги ${bookData.value.title}`,
+  ogTitle: `DnD Vault - ${bookData.value.title}`,
+  ogDescription: `Оглавление и краткое описание книги ${bookData.value.title}`,
   ogType: 'book',
   ogUrl: `https://dndvault.ru/book-${slug.value}/`,
 })
 </script>
 
 <template>
-  <PageTitle>{{ bookData.title }}</PageTitle>
+  <PageTitle>{{ bookData!.title }}</PageTitle>
   <p class="mt-2">
-    {{ bookData.description }}
+    {{ bookData!.description }}
   </p>
   <h2
     class="text-2xl font-semibold mt-4 mb-2 after:h-0.5 after:bg-slate-800 after:block">
@@ -73,7 +54,7 @@ useSeoMeta({
     <li v-for="chapter in chapters">
       <ChapterLink
         :title="chapter.title"
-        :book-slug="bookData.slug"
+        :book-slug="bookData!.slug"
         :chapter-slug="chapter.slug" />
     </li>
   </ul>
