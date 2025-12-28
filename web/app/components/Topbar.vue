881c4e2c25101d7d63d015ca99cf6cace072df52
Denis Bezykornov <archidevil52@gmail.com>
2025-12-28 22:19:45 +0300

    Add social links

--- web/app/components/Topbar.vue
+++ web/app/components/Topbar.vue
@@ -1,9 +1,32 @@
 <template>
-  <nav class="my-2 md:my-8 py-2 md:py-4 flex flex-row gap-8 border-b-2 font-semibold">
-    <NuxtLink
-      class="text-xl underline-offset-4 decoration-2 hover:underline"
-      to="/">
-      Каталог материалов
-    </NuxtLink>
+  <nav
+    class="my-2 md:my-8 py-2 md:py-4 flex flex-row gap-8 border-b-2 font-semibold">
+    <div class="flex-grow h-9 flex flex-row gap-2 items-center">
+      <div class="size-9">
+        <a href="/">
+          <img
+            src="/android-chrome-192x192.png"
+            class="size-full" />
+        </a>
+      </div>
+      <NuxtLink
+        class="text-xl underline-offset-4 decoration-2 hover:underline"
+        to="/">
+        Материалы
+      </NuxtLink>
+    </div>
+    <div class="grow-0 flex flex-row gap-2 items-center">
+      <span class="text-xl">Наши соцсети:</span>
+      <div class="size-9">
+        <a href="https://t.me/dungeons_ru">
+          <img src="~/assets/images/tg_logo.svg" />
+        </a>
+      </div>
+      <div class="size-9">
+        <a href="https://vk.com/dungeons_ru">
+          <img src="~/assets/images/vk_logo.svg" />
+        </a>
+      </div>
+    </div>
   </nav>
 </template>
