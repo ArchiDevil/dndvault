dfe20d8c507d3e4a5d39f14bc638a91ed4f009d5
Denis Bezykornov <archidevil52@gmail.com>
2025-12-30 20:19:46 +0300

    Remove chapters cache as it requires smarter mechanism

--- web/server/api/books/[slug]/chapters.ts
+++ web/server/api/books/[slug]/chapters.ts
@@ -14,45 +14,42 @@ export type ChapterData = {
   children: ChapterData[]
 }
 
-export default defineCachedEventHandler(
-  async (event): Promise<ChapterData[]> => {
-    const {staticToken} = useRuntimeConfig()
-    const bookSlug = getRouterParam(event, 'slug')
-    const bookData = await $fetch(`/api/books/${bookSlug}`)
+export default defineEventHandler(async (event): Promise<ChapterData[]> => {
+  const {staticToken} = useRuntimeConfig()
+  const bookSlug = getRouterParam(event, 'slug')
+  const bookData = await $fetch(`/api/books/${bookSlug}`)
 
-    const {data} = await $fetch<{data: DirectusChapter[]}>(
-      `/api/items/chapters`,
-      {
-        headers: {
-          Authorization: `Bearer ${staticToken}`,
-        },
-        query: {
-          filter: {
-            book_id: {
-              _eq: bookData.id,
-            },
+  const {data} = await $fetch<{data: DirectusChapter[]}>(
+    `/api/items/chapters`,
+    {
+      headers: {
+        Authorization: `Bearer ${staticToken}`,
+      },
+      query: {
+        filter: {
+          book_id: {
+            _eq: bookData.id,
           },
-          sort: 'sort',
-          // Must be in sync with DirectusChapter
-          fields: 'id,title,slug,status,parent',
         },
-      }
-    )
+        sort: 'sort',
+        // Must be in sync with DirectusChapter
+        fields: 'id,title,slug,status,parent',
+      },
+    }
+  )
 
-    const publishedChapters = data.filter((c) => c.status === 'published')
-    return publishedChapters
-      .filter((c) => c.parent === null)
-      .map((c) => ({
-        slug: c.slug,
-        title: c.title,
-        children: publishedChapters
-          .filter((cc) => cc.parent == c.id)
-          .map((cc) => ({
-            slug: cc.slug,
-            title: cc.title,
-            children: [], // only 1 level is supported for now
-          })),
-      }))
-  },
-  {maxAge: 24 * 60 * 60}
-)
+  const publishedChapters = data.filter((c) => c.status === 'published')
+  return publishedChapters
+    .filter((c) => c.parent === null)
+    .map((c) => ({
+      slug: c.slug,
+      title: c.title,
+      children: publishedChapters
+        .filter((cc) => cc.parent == c.id)
+        .map((cc) => ({
+          slug: cc.slug,
+          title: cc.title,
+          children: [], // only 1 level is supported for now
+        })),
+    }))
+})
