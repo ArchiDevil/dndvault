ba1b8b9db683ba03c17f172d3844bc0fa3246f8a
Denis Bezykornov <archidevil52@gmail.com>
2026-01-03 01:09:38 +0300

    Simplify topbar on mobile devices

--- web/app/components/Topbar.vue
+++ web/app/components/Topbar.vue
@@ -16,7 +16,7 @@
       </NuxtLink>
     </div>
     <div class="grow-0 flex flex-row gap-2 items-center">
-      <span class="text-xl">Наши соцсети:</span>
+      <span class="text-xl invisible md:visible">Наши соцсети:</span>
       <div class="size-9">
         <a href="https://t.me/dungeons_ru">
           <img src="~/assets/images/tg_logo.svg" />
