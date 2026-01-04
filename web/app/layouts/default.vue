4ba370c750c57a5df3d854426fc69196c5151ca7
Denis Bezykornov <archidevil52@gmail.com>
2026-01-04 19:38:56 +0300

    Add more SEO tags

--- web/app/layouts/default.vue
+++ web/app/layouts/default.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+useSeoMeta({
+  ogSiteName: 'DnD Vault',
+  twitterCard: 'summary_large_image',
+})
+</script>
+
 <template>
   <div class="container px-4 md:px-12">
     <Topbar />
