628492911ea3d1825a3ad70a060b98243d22b3f9
Denis Bezykornov <archidevil52@gmail.com>
2026-02-08 22:48:33 +0300

    Update sitemap to show all feats

--- web/server/api/__sitemap__/feats.ts
+++ web/server/api/__sitemap__/feats.ts
@@ -1,16 +1,40 @@
 import type {SitemapUrlInput} from '#sitemap/types'
 
+type FeatData = {
+  id: number
+  date_updated: string
+}
+
 export default defineSitemapEventHandler(async () => {
   const {staticToken, backendAddress} = useRuntimeConfig()
 
+  const {data: response} = await $fetch<{data: {count: string}[]}>(
+    `${backendAddress}/items/feats`,
+    {
+      headers: {Authorization: `Bearer ${staticToken}`},
+      query: {'aggregate[count]': '*'},
+    }
+  )
+  const featsCount = Number(response[0].count)
+  let totalFeats: FeatData[] = []
+
+  const itemsPerPage = 100
+  for (let page = 0; page < featsCount / itemsPerPage; page += 1) {
+    const {data: feats} = await $fetch<{data: FeatData[]}>(
+      `${backendAddress}/items/feats`,
+      {
+        headers: {Authorization: `Bearer ${staticToken}`},
+        query: {
+          fields: 'id,date_updated',
+          offset: itemsPerPage * page,
+        },
+      }
+    )
+    totalFeats = totalFeats.concat(feats)
+  }
+
   const output: SitemapUrlInput[] = []
-  const {data: feats} = await $fetch<{
-    data: {id: number; date_updated: string}[]
-  }>(`${backendAddress}/items/feats`, {
-    headers: {Authorization: `Bearer ${staticToken}`},
-    query: {fields: 'id,date_updated'},
-  })
-  for (const feat of feats) {
+  for (const feat of totalFeats) {
     const lastFeatUpdate = new Date(feat.date_updated)
     output.push({
       loc: `/feats/${feat.id}`,
