a4f5612ba067f8dba1292d51243c606cccb6fb8f
Denis Bezykornov <archidevil52@gmail.com>
2026-02-14 20:34:18 +0300

    Improve styling of efota and generic book

--- web/app/components/ChapterToc.vue
+++ web/app/components/ChapterToc.vue
@@ -47,13 +47,14 @@ const filteredChapters = computed(() =>
           v-for="element in filteredChapters"
           :key="element.text">
           <a
-            class="hover:font-semibold py-2 pr-2 hover:bg-slate-300"
+            class="hover:font-semibold py-2 pr-2 hover:bg-slate-300 truncate"
             :href="element.link"
             :class="{
               'pl-2 bg-zinc-50 block': element.level === 2,
               'pl-4 bg-zinc-100 block': element.level === 3,
               'pl-6 bg-zinc-200 hidden lg:block': element.level === 4,
-            }">
+            }"
+            :title="element.text">
             {{ element.text }}
           </a>
         </li>
