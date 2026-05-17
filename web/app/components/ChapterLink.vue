ce2e79d4a8cc328345e1bbd4633beb308fd9b6d3
Denis Bezykornov <archidevil52@gmail.com>
2026-05-17 20:25:29 +0300

    Simplify book TOC layout

--- web/app/components/ChapterLink.vue
+++ web/app/components/ChapterLink.vue
@@ -2,7 +2,7 @@
 import type {TocData} from '~~/server/api/books/[slug]/chapters'
 
 // TODO: support more than 1 level for links
-const {bookSlug, chapterSlug, title} = defineProps<{
+const {bookSlug, chapterSlug, title, toc, children} = defineProps<{
   bookSlug: string
   title: string
   chapterSlug: string
@@ -19,10 +19,14 @@ const {bookSlug, chapterSlug, title} = defineProps<{
 const chapterLink = computed(() => {
   return `/book-${bookSlug}/chapter-${chapterSlug}`
 })
+
+const showSublist = computed(() => {
+  return toc.length > 0 || (children && children.length > 0)
+})
 </script>
 
 <template>
-  <li :class="{'mt-2 first:-mt-2': !child}">
+  <li v-bind="$attrs">
     <a
       class="font-semibold block text-red-900 hover:text-red-950"
       :class="{
@@ -33,7 +37,9 @@ const chapterLink = computed(() => {
       {{ title }}
     </a>
   </li>
-  <ul v-if="toc.length > 0">
+  <ul
+    v-if="showSublist"
+    class="pb-2">
     <li v-for="tocItem in toc">
       <a
         class="text-lg ml-4 hover:font-semibold block"
@@ -41,16 +47,15 @@ const chapterLink = computed(() => {
         {{ tocItem.title }}
       </a>
     </li>
-  </ul>
-  <template v-if="children && children.length > 0">
-    <ul class="ml-4">
+    <template v-if="children && children.length > 0">
       <ChapterLink
         v-for="child in children"
+        class="ml-4"
         :book-slug="bookSlug"
         :title="child.title"
         :toc="child.toc"
         :chapter-slug="child.chapterSlug"
         child />
-    </ul>
-  </template>
+    </template>
+  </ul>
 </template>
--- web/app/components/PageTitle.vue
+++ web/app/components/PageTitle.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1 class="font-semibold text-3xl md:text-5xl pb-4 md:pb-8">
+  <h1 class="font-semibold text-2xl md:text-3xl pb-4 md:pb-4">
     <slot />
   </h1>
 </template>
