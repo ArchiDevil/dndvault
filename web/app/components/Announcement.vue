c9a8a1286b4ff2f7c8833437c1f6ac91838869b6
Denis Bezykornov <archidevil52@gmail.com>
2026-06-05 20:18:54 +0300

    Hide announcement line on printing

--- web/app/components/Announcement.vue
+++ web/app/components/Announcement.vue
@@ -7,7 +7,7 @@ const {data: announcement} = useFetch('/api/announcement')
     v-if="
       announcement && announcement.text !== null && announcement.text.length > 0
     "
-    class="py-2 w-full bg-amber-300 mx-auto px-4 md:px-12 text-center">
+    class="py-2 w-full bg-amber-300 mx-auto px-4 md:px-12 text-center print:hidden">
     {{ announcement.text }}
   </div>
 </template>
